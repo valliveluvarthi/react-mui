b36a5423a19417745ef27acfb908eba79136522d
VELUVARTHI VALLI <simhavalli25397@gmail.com>
2023-04-03 00:28:21 +0530

    added getvalues

--- cra_JS/src/sections/@dashboard/user/user-content/POMAccess.js
+++ cra_JS/src/sections/@dashboard/user/user-content/POMAccess.js
@@ -76,6 +76,7 @@ export default function POMAccess() {
         setValue,
         handleSubmit,
         formState: { isSubmitting, isValid },
+        getValues,
     } = methods;
 
     const values = watch();
